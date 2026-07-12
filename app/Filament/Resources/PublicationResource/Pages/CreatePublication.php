b806cc43b54a21f5f56078e1dc82fe5162cefd57
Adnan Amin, Ph.D. <geoamins@gmail.com>
2026-07-12 20:16:52 +0500

    ..

--- app/Filament/Resources/PublicationResource/Pages/CreatePublication.php
+++ app/Filament/Resources/PublicationResource/Pages/CreatePublication.php
@@ -3,9 +3,24 @@
 namespace App\Filament\Resources\PublicationResource\Pages;
 
 use App\Filament\Resources\PublicationResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Js;
 
 class CreatePublication extends CreateRecord
 {
     protected static string $resource = PublicationResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return PublicationResource::getUrl('index');
+    }
+
+    protected function getCancelFormAction(): Action
+    {
+        return Action::make('cancel')
+            ->label(__('filament-panels::resources/pages/create-record.form.actions.cancel.label'))
+            ->alpineClickHandler('window.location.href = '.Js::from(PublicationResource::getUrl('index')))
+            ->color('gray');
+    }
 }
--- app/Filament/Resources/PublicationResource/Pages/EditPublication.php
+++ app/Filament/Resources/PublicationResource/Pages/EditPublication.php
@@ -3,9 +3,33 @@
 namespace App\Filament\Resources\PublicationResource\Pages;
 
 use App\Filament\Resources\PublicationResource;
+use Filament\Actions\Action;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Js;
 
 class EditPublication extends EditRecord
 {
     protected static string $resource = PublicationResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make()
+                ->successRedirectUrl(PublicationResource::getUrl('index')),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return PublicationResource::getUrl('index');
+    }
+
+    protected function getCancelFormAction(): Action
+    {
+        return Action::make('cancel')
+            ->label(__('filament-panels::resources/pages/edit-record.form.actions.cancel.label'))
+            ->alpineClickHandler('window.location.href = '.Js::from(PublicationResource::getUrl('index')))
+            ->color('gray');
+    }
 }
